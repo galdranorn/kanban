b204fa35cd416d0f0e2fba3b13b5f39e38c695d7
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-10-28 15:48:31 +0100

    Modify App.js

--- js/App.js
+++ js/App.js
@@ -1,12 +1,37 @@
-// OGÓLNA FUNKCJA
-function randomString() {
-	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
-	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
-	}
-	return str;
+// API urls
+const baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+const myHeaders = {
+  'X-Client-Id': 3575,
+  'X-Auth-Token': '16403045ea8cfafe532e2b1d7dbc399d'
+};
+
+// ASK SERVER ABOUT ARRAY CONTENT
+fetch(baseUrl + '/board', { headers: myHeaders })
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(resp) {
+    setupColumns(resp.columns);
+  });
+
+// FUNCTION FOR CREATING COLUMN
+function setupColumns(columns) {
+	columns.forEach(function(column) {
+		  var col = new Column(column.id, column.name);
+		board.addColumn(col);
+		setupCards(col, column.cards);
+	});
+  }
+
+//  FUNCTION FOR CREATING CARDS
+  function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
 }
+
+// OGÓLNA FUNKCJA
 
 function generateTemplate(name, data, basicElement) {
   	var template = document.getElementById(name).innerHTML;
@@ -17,21 +42,3 @@ function generateTemplate(name, data, basicElement) {
   
   	return element;
 }
-
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
-
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
-
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
-
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
